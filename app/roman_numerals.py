7e3feb10bb20869e1f19df51a9b25e346a22bb85
Arturo Mora-Rioja <arturomora@gmail.com>
2024-09-06 09:04:30 +0200

    Specification checks added

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,4 +1,51 @@
+"""
+Conversion from Roman numeral to decimal
+
+v1.0.0 August 2024
+v1.1.0 September 2024. Specification checks added
+"""
+__author__ = "Arturo Mora-Rioja"
+__version__ = "1.1.0"
+
 def roman_to_decimal(number: str) -> int:
+    error_message = 'Badly formed Roman numeral'
+
+    # Invalid symbols are removed from the string
+    clean_number = []
+    for letter in number:
+        if letter in ['M', 'D', 'C', 'L', 'X', 'V', 'I']:
+            clean_number.append(letter)
+    number = ''.join(clean_number)
+
+    # V, L and D can never be repeated
+    if number.count('V') > 1 or number.count('L') > 1 or number.count('D') > 1:
+        raise Exception(error_message)
+
+    # No digit can be repeated more than 3 times in a row
+    if 'MMMM' in number or 'CCCC' in number or 'XXXX' in number or 'IIII' in number:
+        raise Exception(error_message)
+
+    # D cannot precede M
+    if 'DM' in number:
+        raise Exception(error_message)
+
+    # L cannot precede M, D or C
+    if 'LM' in number or 'LD' in number or 'LC' in number:
+        raise Exception(error_message)
+
+    # V cannot precede M, D, C, L or X
+    if 'VM' in number or 'VD' in number or \
+       'VC' in number or 'VL' in number or 'VX' in number:
+        raise Exception(error_message)
+
+    # X cannot precede D
+    if 'XD' in number:
+        raise Exception(error_message)
+
+    # I cannot precede M, D, C or L
+    if 'IM' in number or 'ID' in number or \
+       'IC' in number or 'IL' in number:
+        raise Exception(error_message)
 
     # Each letter of the Roman numeral 
     # is translated into its decimal value
--- test/test_roman_numerals.py
+++ test/test_roman_numerals.py
@@ -2,7 +2,7 @@ import pytest
 from app.roman_numerals import roman_to_decimal
 
 @pytest.mark.parametrize('roman_number,decimal_number', [
-    ('', 0),            
+    ('', 0),                # Valid lower boundary
     ('M', 1000),            
     ('MD', 1500),            
     ('MCD', 1400),
@@ -14,7 +14,48 @@ from app.roman_numerals import roman_to_decimal
     ('IX', 9),
     ('MMXXIII', 2023),
     ('MMJXXOIII', 2023),
-    ('MXDIVII', 1496),
+    ('MMMCMXCIX', 3999)     # Valid upper boundary
 ])
-def test_roman_numerals(roman_number, decimal_number):
+def test_roman_numerals_passes(roman_number, decimal_number):
     assert roman_to_decimal(roman_number) == decimal_number
+
+@pytest.mark.parametrize('roman_number', [
+    ('MXDIVII'),
+    ('VV'),
+    ('MDVVI'),
+    ('MDVXVI'),
+    ('LL'),
+    ('MDLLX'),
+    ('MDLXLV'),
+    ('DD'),
+    ('MDDCX'),
+    ('MDCDXVI'),
+    ('IIII'),
+    ('XVIIII'),
+    ('CLIIIIX'),
+    ('XXXX'),
+    ('MDXXXX'),
+    ('MCXXXXV'),
+    ('CCCC'),
+    ('MDCCCC'),
+    ('MCCCCDV'),
+    ('MMMM'),        # Invalid upper boundary
+    ('MMMMCL'),
+    ('DM'),
+    ('LM'),
+    ('LD'),
+    ('LC'),
+    ('VM'),
+    ('VD'),
+    ('VC'),
+    ('VL'),
+    ('VX'),
+    ('XD'),
+    ('IM'),
+    ('ID'),
+    ('IC'),
+    ('IL')
+])
+def test_badly_formed_roman_numerals(roman_number):
+    with pytest.raises(Exception):
+        roman_to_decimal(roman_number)
